2ba8dff75cc056978a4a0f66ea84ea346675b5a7
sangjin-hash <sangjin970301@gmail.com>
2026-02-26 21:47:38 +0900

    [FIX] Observable+Task 접근제어 -> public으로 수정

--- Market09/Shared/Sources/Rx/Observable+Task.swift
+++ Market09/Shared/Sources/Rx/Observable+Task.swift
@@ -12,7 +12,7 @@ extension Observable {
     /// async throws 함수를 Observable로 변환
     /// - Parameter work: async throws 클로저
     /// - Returns: 성공 시 onNext, 실패 시 onError를 방출하는 Observable
-    static func task(_ work: @escaping () async throws -> Element) -> Observable<Element> {
+    public static func task(_ work: @escaping () async throws -> Element) -> Observable<Element> {
         Observable.create { observer in
             let task = Task {
                 do {
