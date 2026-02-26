48a78c77d421589f46b9b1357e7c508958bc6ffc
sangjin-hash <sangjin970301@gmail.com>
2026-02-26 21:50:51 +0900

    [FEAT] DataAssembly, DomainAssembly DI 등록 구현

--- Market09/Data/Sources/Assembly/DataAssembly.swift
+++ Market09/Data/Sources/Assembly/DataAssembly.swift
@@ -5,6 +5,7 @@
 //  Created by Sangjin Lee
 //
 
+import Foundation
 import Swinject
 import Domain
 
@@ -13,10 +14,35 @@ public final class DataAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
-        // TODO: RepositoryImpl, Client, DataSource 의존성 등록
-//        container.register(GroupBuyRepository.self) { _ in
-//            GroupBuyRepositoryImpl()
-//        }
-//        .inObjectScope(.container)
+        
+        // MARK: - KeychainClient
+        
+        container.register(KeychainClient.self) { _ in
+            KeychainClientImpl()
+        }.inObjectScope(.container)
+        
+        // MARK: - Auth
+        
+        container.register(AuthRemoteDataSource.self) { _ in
+            guard let urlString = Bundle.main.infoDictionary?["SUPABASE_URL"] as? String,
+                  let url = URL(string: urlString),
+                  let key = Bundle.main.infoDictionary?["SUPABASE_ANON_KEY"] as? String else {
+                fatalError("Supabase 설정이 Info.plist에 없습니다. Secrets.xcconfig을 확인하세요.")
+            }
+            return AuthRemoteDataSourceImpl(supabaseURL: url, supabaseKey: key)
+        }.inObjectScope(.container)
+        
+        container.register(AuthLocalDataSource.self) { resolver in
+            AuthLocalDataSourceImpl(
+                keychainClient: resolver.resolve(KeychainClient.self)!
+            )
+        }.inObjectScope(.container)
+
+        container.register(AuthRepository.self) { resolver in
+            AuthRepositoryImpl(
+                remoteDataSource: resolver.resolve(AuthRemoteDataSource.self)!,
+                localDataSource: resolver.resolve(AuthLocalDataSource.self)!
+            )
+        }.inObjectScope(.container)
     }
 }
