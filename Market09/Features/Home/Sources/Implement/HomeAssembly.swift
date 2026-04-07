0c37a7f15027b2f039643034faf4e666a3f2d442
sangjin-hash <sangjin970301@gmail.com>
2026-04-08 01:51:17 +0900

    [FEAT] 인기 공구 Top 10 기능 추가

--- Market09/Features/Home/Sources/Implement/HomeAssembly.swift
+++ Market09/Features/Home/Sources/Implement/HomeAssembly.swift
@@ -18,7 +18,6 @@ public final class HomeAssembly: Assembly {
         container.register(HomeReactor.Factory.self) { r in
             HomeReactor.Factory(dependency: .init(
                 fetchPostsListUseCase: r.resolve(),
-                fetchTop10PostsUseCase: r.resolve(),
                 likePostUseCase: r.resolve(),
                 cancelLikePostUseCase: r.resolve(),
                 userStore: r.resolve()
@@ -33,10 +32,25 @@ public final class HomeAssembly: Assembly {
         }
         .inObjectScope(.graph)
         
+        container.register(HomeTop10Reactor.Factory.self) { r in
+            HomeTop10Reactor.Factory(dependency: .init(
+                fetchTop10PostsUseCase: r.resolve()
+            ))
+        }
+        .inObjectScope(.graph)
+        
+        container.register(HomeTop10ViewController.Factory.self) { r in
+            HomeTop10ViewController.Factory(dependency: .init(
+                reactor: r.resolve(HomeTop10Reactor.Factory.self)!.create()
+            ))
+        }
+        .inObjectScope(.graph)
+        
         container.register(HomeCoordinator.self) { (r, navigationController: UINavigationController) in
             HomeCoordinatorImpl(
                 navigationController: navigationController,
-                viewController: r.resolve(HomeViewController.Factory.self)!.create()
+                homeViewController: r.resolve(HomeViewController.Factory.self)!.create(),
+                homeTop10ViewController: r.resolve(HomeTop10ViewController.Factory.self)!.create()
             )
         }
         .inObjectScope(.graph)
--- Market09/Features/Home/Sources/Implement/HomeCoordinatorImpl.swift
+++ Market09/Features/Home/Sources/Implement/HomeCoordinatorImpl.swift
@@ -27,7 +27,8 @@ final class HomeCoordinatorImpl: HomeCoordinator {
     
     // MARK: - Reactor
     
-    private let viewController: HomeViewController
+    private let homeViewController: HomeViewController
+    private let homeTop10ViewController: HomeTop10ViewController
     private let disposeBag = DisposeBag()
 
 
@@ -35,17 +36,19 @@ final class HomeCoordinatorImpl: HomeCoordinator {
 
     public init(
         navigationController: UINavigationController,
-        viewController: HomeViewController
+        homeViewController: HomeViewController,
+        homeTop10ViewController: HomeTop10ViewController
     ) {
         self.navigationController = navigationController
-        self.viewController = viewController
+        self.homeViewController = homeViewController
+        self.homeTop10ViewController = homeTop10ViewController
     }
 
 
     // MARK: - Start
 
     public func start() {
-        guard let reactor = self.viewController.reactor else { return }
+        guard let reactor = self.homeViewController.reactor else { return }
         
         reactor.pulse(\.$loginConfirmed)
             .filter { $0 }
@@ -55,6 +58,21 @@ final class HomeCoordinatorImpl: HomeCoordinator {
             })
             .disposed(by: self.disposeBag)
         
-        self.navigationController.pushViewController(self.viewController, animated: true)
+        reactor.pulse(\.$showTop10)
+            .filter { $0 }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                self?.showTop10()
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.navigationController.pushViewController(self.homeViewController, animated: true)
+    }
+    
+    
+    // MARK: - Top10
+    
+    func showTop10() {
+        self.navigationController.pushViewController(self.homeTop10ViewController, animated: true)
     }
 }
